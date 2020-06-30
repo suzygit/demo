083cfd1b4d62e6719dd918462d2f758d52283a60
suziyi <1>
2020-06-30 17:35:12 +0800

    suzy:demo

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        int[] arr  = {3,5,1,7,4,9};
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        for(int k = 0;k<arr.length;k++){
+            System.out.println(arr[k]);
+        }
+    }
+
+}
